f957c5a78e404029f021e061b9103d2fb7b59155
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-05 14:25:46 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,22 @@ class App extends React.Component {
       this.setState({filters:{...this.state.filters,type:event.target.selected}});
   }
 
+  handlePets = () => {
+    const API = '/api/pets';
+    const selectedVal = this.state.filters.type;
+    var fetchAPI = '';
+    if (selectedVal === 'all') {
+      fetchAPI = API;
+    } else {
+      fetchAPI = API + '?type=' + selectedVal;
+    }
+    fetch(fetchAPI).then(response => response.json).then(data => this.setState({pets:data}));
+  }
+
+  handleAdopt = () => {
+
+  }
+
   render() {
     return (
       <div className="ui container">
