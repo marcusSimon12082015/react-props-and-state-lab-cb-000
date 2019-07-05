f7120857fd215a7c878f39321605eb7f1d083697
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-05 14:27:16 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -7,7 +7,7 @@ class Pet extends React.Component {
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.name}
           </a>
           <div className="meta">
             <span className="date">PET TYPE</span>
